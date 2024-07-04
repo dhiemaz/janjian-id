5a71c2d2aab0ebe40d92bb34fa33efe2ec74d39a
Dimas <prawira.dimas.yudha@gmail.com>
2024-07-04 21:18:27 +0700

    fixing visibility navbar.

--- src/components/navbar-no-login/navbar_no_login.tsx
+++ src/components/navbar-no-login/navbar_no_login.tsx
@@ -75,7 +75,7 @@ export default function NavbarNoLogin() {
                     Log In
                 </Link>
                 <Link href="/registration"
-                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 visible lg:invisible">
+                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 invisible lg:visible">
                     Get Started
                 </Link>
                 <ul className="menu menu-horizontal space-x-2 md:space-x-0 rtl:space-x-reverse">
